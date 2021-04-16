95690c4b6276aa1e42e393f186c66d11ba673514
Aditya Nugraha <adityanugraha795@gmail.com>
2021-04-16 23:01:46 +0800

    filter jk, jurusan

--- api/siswa/bisa filter agama tingkat jurusan.php
+++ api/siswa/bisa filter agama tingkat jurusan.php
@@ -19,12 +19,9 @@ include dirname(dirname(__FILE__)).'/db/Db.class.php';
 $db = new Db();
 $limit = isset($_GET['limit']) ? (int) $_GET['limit'] : 0;
 //$name = isset($_GET['name']) ? $_GET['name'] : '';
-$jurusan = isset($_GET['jurusan']) ? $_GET['jurusan'] : '';
-$jk = isset($_GET['jk']) ? $_GET['jk'] : '';
 $tingkat = isset($_GET['tingkat']) ? $_GET['tingkat'] : '';
 $agama = isset($_GET['agama']) ? $_GET['agama'] : '';
-
-$jk = isset($_GET['jk']) ? $_GET['jk'] : '';
+$jurusan = isset($_GET['jurusan']) ? $_GET['jurusan'] : '';
 
 $sql_limit = '';
 if (!empty($limit)) {
@@ -35,24 +32,14 @@ if (!empty($name)) {
     $sql_name = ' where tingkat LIKE \'%'.$name.'%\' ';
 }
 
-$jurusan_name = '';
-if (!empty($jurusan)) {
-    $jurusan_name = ' where jurusan LIKE \'%'.$jurusan.'%\' ';
-}
 $tingkat_name = '';
 if (!empty($tingkat)) {
     $tingkat_name = ' where tingkat LIKE \'%'.$tingkat.'%\' ';
 }
 
-$jk_name = '';
-if (!empty($jk)) {
-    $jk_name = ' where jk LIKE \'%'.$jk.'%\' ';
-}
-
-
 $agama_name = '';
 if (!empty($agama)) {
-    $agama_name = ' where agama LIKE \'%'.$agama.'%\' and tingkat LIKE \'%'.$tingkat.'%\' and jurusan LIKE \'%'.$jurusan.'%\'and jk LIKE \'%'.$jk.'%\' ';
+    $agama_name = ' where agama LIKE \'%'.$agama.'%\' and tingkat LIKE \'%'.$tingkat.'%\' and jurusan LIKE \'%'.$jurusan.'%\' ';
 }
 
 
@@ -61,7 +48,7 @@ if (!empty($agama)) {
 
 //$cat_list = $db->query('select * from categories '.$sql_name.' '.$sql_limit);
 //$cat_list = $db->query('select * from data_siswa');
-$cat_list = $db->query('select * from data_siswa  '.$agama_name.'' .$tingkat_name.''.$jurusan_name.''.$jk_name.''.$sql_limit);
+$cat_list = $db->query('select * from data_siswa  '.$agama_name.' '.$sql_limit);
 $arr = array();
 $arr['info'] = 'success';
 $arr['num'] = count($cat_list);
