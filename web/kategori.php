deef2457b2c9c51e7149562c7b0158df180f6ee0
Aditya Nugraha <adityanugraha795@gmail.com>
2021-04-16 22:15:06 +0800

    edit nama tampil api

--- web/kategori.php
+++ web/kategori.php
@@ -10,7 +10,7 @@ include 'inc.php';
     
     <body>
     <?php
-    $api_categories_list = $api_url.'/categories/news.php?agama=Islam&tingkat=10&api_key='.$api_key;
+    $api_categories_list = $api_url.'/siswa/index.php?agama=Islam&tingkat=10&api_key='.$api_key;
     $json_list = @file_get_contents($api_categories_list);
     ?>
     <h1>Kategori</h1>
